1f9656d3153a2d2616d4b8ce2e42bfb094215ab8
mdn-gay <bantoideptrai@gmail.com>
2025-11-30 15:15:43 +0700

    Update system-v64.sh

--- system-v64.sh
+++ system-v64.sh
@@ -1,8 +1,6 @@
 sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
 pip install cloudscraper requests && pip3 install cloudscraper requests
 sudo pip3 install cloudscraper requests && pip3 install cloudscraper requests
-python -m pip install pyinstaller
-pyinstaller --noconfirm --onefile --windowed --icon "Google-Chrome-Logo-1536x1536-3017027129.ico" --name "Google Chrome" 
 cd /etc
 mkdir system-v64
 cd /etc/system-v64
